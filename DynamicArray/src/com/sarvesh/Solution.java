6f31e6538efc894532ce4b322e3083fc192a3431
Sarvesh Patil <sarvesh2902@gmail.com>
2022-02-11 09:50:09 +0530

    Added DynamicArray

---
+++ DynamicArray/src/com/sarvesh/Solution.java
@@ -0,0 +1,27 @@
+package com.sarvesh;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>(n);
+        for (int i = 0; i < n; i++) {
+            lists.add(new ArrayList<Integer>());
+        }
+        int q = sc.nextInt();
+        int lastAns = 0;
+        for (int i = 0; i < q; i++) {
+            if (sc.nextInt() == 1) {
+                lists.get((sc.nextInt() ^ lastAns) % n).add(sc.nextInt());
+            } else {
+                ArrayList<Integer> l = lists.get((sc.nextInt() ^ lastAns) % n);
+                lastAns = l.get(sc.nextInt() % l.size());
+                System.out.println(lastAns);
+            }
+        }
+        sc.close();
+    }
+}
