4cc9198eb766edf85544ab2326ecf8da624a5ffc
Sarvesh Patil <sarvesh2902@gmail.com>
2022-02-11 08:34:11 +0530

    Added Arrays-DS

---
+++ Arrays-DS/src/com/sarvesh/Solution.java
@@ -0,0 +1,19 @@
+package com.sarvesh;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[scan.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+        scan.close();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
